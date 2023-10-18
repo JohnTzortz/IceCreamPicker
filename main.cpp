636b07d3a497fe64a034f62d075708010dd0849a
Ioannis Tzortzakis <ioannis.tzortzak@gmail.com>
2023-10-18 14:12:58 +0300

    Implemented wrting in file, recomendation func and exit func

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ struct IceCream {
     std::string name;
     int timesTried =0;
     int rating =0;
-    bool exist = true;
+    bool exist = true;  //maybe there no ice cream in that place in the shelf
     std::pair<int, int> location; // Assuming coordinates as (row, column)
 };
 
@@ -26,11 +26,14 @@ std::vector<IceCream> loadIceCreamData() {
             iceCreams.push_back({name, timesTried, rating, exist, {row, col}});
         }
         file.close();
+    }else{
+        std::cerr<< "An error has accured regarding the file."<<std::endl;
+        std::exit(EXIT_FAILURE);
     }
     return iceCreams;
 }
 
-void saveIceCreamData(const std::vector<IceCream>& iceCreams) {
+void saveIceCreamData(std::vector<IceCream>& iceCreams) {
     std::ofstream file("ice_cream_data.txt");
     if (file.is_open()) {
         for (const auto& iceCream : iceCreams) {
@@ -138,6 +141,14 @@ std::vector<IceCream> init_vector(std::pair<int, int> coords){
     return v;
 }
 
+/**
+ * r = recomendations
+ * t = top3
+ * e = edit
+ * p = print vector
+ * x = exit
+ * else, do nothing.
+*/
 int userStringInput(std::vector<IceCream> v) {
     std::string userInput;
     std::cout << ">> ";
@@ -154,9 +165,14 @@ int userStringInput(std::vector<IceCream> v) {
             }else
                 if(userInput == "p"){
                     return 3;
-                }else{
-                    return 4;
-                    }
+                }else
+                    if(userInput == "x"){
+                        return 4;
+                    }else
+                        if(userInput == "s"){
+                            return 5;
+                        }else
+                            return 6;
 
 
 }
@@ -172,22 +188,99 @@ void print_vector(std::vector<IceCream>& iceCreams) {
         } 
 }
 
-void recomendation(std::vector<IceCream>& iceCreams){}
+void recomendation(std::vector<IceCream>& iceCreams){
+    
+    std::vector<IceCream> untastedIceCreams;
 
-void editVector(std::vector<IceCream>& iceCreams){}
+    for(IceCream& IceCream : iceCreams){
+        if(IceCream.timesTried == 0){
+            untastedIceCreams.push_back(IceCream);
+        }
+    }
+            
 
-void top3(std::vector<IceCream>& iceCreams){}
+    bool accepted = false;
+
+    while (!accepted){
+        int randomNumber = std::rand() % (untastedIceCreams.size());
+        std::cout<< "\nI recomend you the flavor at the row #"<<untastedIceCreams.at(randomNumber).location.first;
+        std::cout<<" and column #"<<untastedIceCreams.at(randomNumber).location.second<<".\n";
+        std::cout<< "The name is \'"<<untastedIceCreams.at(randomNumber).name<<"\'.\nWould you like to try it?\n";
+        std::cout<< "Type y for yes, n for no.\n";
+
+        std::string input;
+        std::getline(std::cin, input);
+        
+        if(input == "y"){
+            accepted = true;
+            std::pair<int, int> tastePair = untastedIceCreams.at(randomNumber).location;
+            
+            for(int i=0; i<iceCreams.size(); i++){
+                if(iceCreams.at(i).location == tastePair){
+                    iceCreams.at(i).timesTried++;
+                    break;
+                }
+            }
+
+            
+        }
+        else
+            if(input == "n")
+                std::cout<< "\nI'll recommend you something else then."<< std::endl;
+            else{
+                std::cout<<"\nIncorrect answer. I'll show you another option."<<std::endl;
+            }
+    
+    }
+
+    
+    std::cout<< "\n\nGood choice! Enjoy!\nDon't forget to save before you go :)\n\n";
+
+}
+
+void editVector(std::vector<IceCream>& iceCreams){
+
+
+}
+
+void top3(std::vector<IceCream>& iceCreams){
+
+
+}
 
 void doNothing(std::vector<IceCream>& iceCreams){
 
 }
+
+void finishProgram(std::vector<IceCream>& iceCreams){
+    std::cout<< "\nEnjoy your ice cream :)\n"<<std::endl;
+    std::exit(EXIT_SUCCESS);
+}
+
+bool passValuesToFile(std::ofstream& outFile, std::vector<IceCream> iceCreams){
+
+    if(outFile.is_open()){
+        for (const IceCream& iceCream : iceCreams) {
+            outFile << "\"" << iceCream.name << "\" " << iceCream.timesTried << " "
+                        << iceCream.rating << " " << iceCream.exist << " "
+                        << iceCream.location.first<< " " << iceCream.location.second << "\n";
+        }        
+    
+    outFile.close();
+
+    }else
+        return false;
+
+    return true;
+}
+
 
 int main() {
     std::srand(std::time(0));
 
     std::vector<IceCream> iceCreamVector;
 
-    void (*functionality[])(std::vector<IceCream>&) = {recomendation, top3, editVector, print_vector, doNothing};
+    void (*functionality[])(std::vector<IceCream>&) = {recomendation, top3, editVector, print_vector, finishProgram, saveIceCreamData, doNothing};
 
     if(!fileExists("ice_cream_data.txt")){
         std::cout<< "File \"ice_cream_data.txt\" does not exist!"<<std::endl;
@@ -200,16 +293,23 @@ int main() {
         std::cout<<"\nDefault settings applied. Name = No-name, Times tasted = 0, Rating = 0, and Exist = yes.\n"<<"You can edit these attributes later on.\n"<<std::endl;
 
         iceCreamVector = init_vector(layout);
-        
+
+        if(!passValuesToFile(outFile, iceCreamVector)){
+            std::cerr<< "An error has accured regarding the file."<<std::endl;
+            std::exit(EXIT_FAILURE);
+            }
+
     }else{
         std::cout<< "File already exists. \nLoading content of file...\n"<<std::endl;
+        
         iceCreamVector= loadIceCreamData();
     }
 
 
     while (true)
     {
-        std::cout<<"Type \'r\' for a recomendation of a new ice cream, \nType \'t\' for the top 3 ice creams you tasted already,\nType \'e\' to edit any attribute of a spesific ice cream.\nType \'p\' to print all the ice creams with all their attributes.\n"<<std::endl;
+        std::cout<<"Type \'r\' for a recomendation of a new ice cream, \nType \'t\' for the top 3 ice creams you tasted already,\nType \'e\' to edit any attribute of a spesific ice cream.\nType \'p\' to print all the ice creams with all their attributes.\nType \'s\' to save. \nType \'x\' to exit the program (Don't forget to save before you go ;) ).\n"<<std::endl;
+        
         functionality[userStringInput(iceCreamVector)](iceCreamVector);
     }
     
